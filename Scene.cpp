9de86a773ec0d4d0b738f2ef27bd7a661d512250
Mariano Belgrano <mbelgrano@gmail.com>
2024-10-30 23:47:04 -0300

    movi la fuente al directorio assets/fonts

--- Scene.cpp
+++ Scene.cpp
@@ -3,7 +3,7 @@
 
 Scene::Scene()
 {
-  _font.loadFromFile("assets/fonts/MONOCOQUE_FUENTE.tft");
+  _font.loadFromFile("assets/fonts/MONOCOQUE_FUENTE.ttf");
   _text.setFont(_font);
   _text.setCharacterSize(24);
   _bg.loadFromFile("assets/sprites/bg-maps-1942.png");
